95bc50440c19a8ee7109a6bf8f8ac5d007fb2a60
PRIYANKA Pandey <pandey@pdx.edu>
2020-02-22 18:50:31 -0800

    net gun

--- src/MyPlayer/Shooter.java
+++ src/MyPlayer/Shooter.java
@@ -4,24 +4,32 @@ import battlecode.common.*;
 
 public class Shooter extends Building {
 
-    public Shooter(RobotController r) throws GameActionException {
-        super(r);
+    public Shooter(RobotController rc) throws GameActionException {
+        super(rc);
     }
 
+    @Override
     public void takeTurn() throws GameActionException {
-        super.takeTurn();
+        while (true) {
+            RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED,rc.getTeam());
+            for (RobotInfo r : robots){
+                if(r.getType() == RobotType.DELIVERY_DRONE && rc.canShootUnit(r.ID)) {
+                    rc.shootUnit(r.ID);
+                    break;
+            //super.takeTurn();
 
         // shoot nearby enemies
-        Team enemy = rc.getTeam().opponent();
-        RobotInfo[] enemiesInRange = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, enemy);
+        //Team enemy = rc.getTeam().opponent();
+       // RobotInfo[] enemiesInRange = rc.senseNearbyRobots(GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, enemy);
 
-        for (RobotInfo e : enemiesInRange) {
-            if (e.type == RobotType.DELIVERY_DRONE) {
-                if (rc.canShootUnit(e.ID)){
-                    rc.shootUnit(e.ID);
-                    break;
+        //for (RobotInfo e : enemiesInRange) {
+          //  if (e.type == RobotType.DELIVERY_DRONE) {
+               // if (rc.canShootUnit(e.ID)){
+                  //  rc.shootUnit(e.ID);
+                  //  break;
                 }
             }
+            Clock.yield();
         }
     }
 }
