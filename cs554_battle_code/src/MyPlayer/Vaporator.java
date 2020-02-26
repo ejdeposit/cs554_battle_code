6ce8da95c480328f5aaee14060b3610b54de0be9
nishnareddy1 <aletinishna0723@gmail.com>
2020-02-25 16:00:47 -0800

    updating .gitignore

--- cs554_battle_code/src/MyPlayer/Vaporator.java
+++
@@ -1,9 +0,0 @@
-package MyPlayer;
-
-import battlecode.common.*;
-
-public class Vaporator extends Building {
-    public Vaporator(RobotController r) throws GameActionException {
-        super(r);
-    }
-}
